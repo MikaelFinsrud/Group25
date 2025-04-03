cd9a529096c24fbc3d647c681e6033ae1a33262c
MikaelFinsrud <mikael.lauvlid@hotmail.no>
2025-04-03 11:28:43 +0200

    Added connection to db + fixed route path issue

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mariadb = require('mariadb');
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
 
+// General const values
+const PORT = process.env.PORT || 5000;
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASS = process.env.DB_PASS || '';
+const DB_NAME = process.env.DB_NAME || 'electromart_3nf';
+
+// Routers
 const authenticationRoutes = require(path.join(__dirname, 'routes', 'authentication'));
 const categoriesRoutes = require(path.join(__dirname, 'routes', 'categories'));
 const ordersRoutes = require(path.join(__dirname, 'routes', 'orders'));
 const productsRoutes = require(path.join(__dirname, 'routes', 'products'));
 
-// Routers
+// Use routers
 app.use('/api/authentication', authenticationRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/orders', ordersRoutes);
 app.use('/api/products', productsRoutes);
 
 // Fallback for no API matches
-app.use('/api/*',(req, res, next) => {
+app.use('/api/*any',(req, res, next) => {
   const status = 404;
   const message = 'Not found';
 
@@ -38,12 +45,33 @@ app.use((err, req, res, next) => {
 });
 
 // Serve frontend
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 app.get('/*any', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+// Create database pool (not a connection, a pool of connections)
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  connectionLimit: 5  // Number of concurrent connections
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
+  console.log("Testing the database connection..");
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT * FROM products");
+    console.log("Products: ", rows);
+  }
+  catch {
+    console.error("Database connection failed!");
+  }
 });
+
+module.exports = pool;
